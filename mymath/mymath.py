69d91e5906b972086bb8c1290d984f587da2f500
Kostas Georgiadis <kongeor@gmail.com>
2013-05-07 17:00:52 +0300

    Add is_odd function

--- mymath/mymath.py
+++ mymath/mymath.py
@@ -1,2 +1,5 @@
 def add(x,y):
     return x + y
+
+def is_odd(x):
+    return x % 2 == 1
--- test.py
+++ test.py
@@ -6,5 +6,8 @@ class TestMath(unittest.TestCase):
     def test_add(self):
         self.assertEqual(mymath.add(5,4), 9)
 
+    def test_is_odd(self):
+        self.assertTrue(mymath.is_odd(9))
+
 if __name__ == '__main__':
     unittest.main()
