2b09c8318db8bc1828cf0afa0d9bdd9d05b33b69
Kostas Georgiadis <kongeor@gmail.com>
2013-05-07 16:54:37 +0300

    Add sub function and test

--- test.py
+++ test.py
@@ -6,5 +6,8 @@ class TestMath(unittest.TestCase):
     def test_add(self):
         self.assertEqual(mymath.add(5,4), 9)
 
+    def test_sub(self):
+        self.assertEqual(mymath.sub(5,4), 1)
+
 if __name__ == '__main__':
     unittest.main()
